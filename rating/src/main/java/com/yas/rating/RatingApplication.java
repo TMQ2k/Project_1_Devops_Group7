006cf2a88620c56238d7493eb756b5b0e6afc7f6
TMQ2k <kentran072009@gmail.com>
2026-03-10 15:10:24 +0700

    feat: Add initial Spring Boot application class for the rating service.

--- rating/src/main/java/com/yas/rating/RatingApplication.java
+++ rating/src/main/java/com/yas/rating/RatingApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+// comment test v1
 @SpringBootApplication(scanBasePackages = {"com.yas.rating", "com.yas.commonlibrary"})
 @EnableConfigurationProperties({ServiceUrlConfig.class, CorsConfig.class})
 public class RatingApplication {
